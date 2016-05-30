65d7ad07f6bb69af798570cda14d3b6bb7d89cce
Jon Ander Besga <jonan.bsg@gmail.com>
2016-05-31 01:24:29 +0200

    more progress with pubnub

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 30/05
 6:33 PM - 19:50 PM - Setting Arch for Python, installing Flask and 
 making first tests
+22:25 PM - 01:23 - Learning about Pubnub, wasting time with the UI and an API that I don't really need. Already have the Pubnub callback.
--- app/app.py
+++ app/app.py
@@ -1,26 +1,105 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 import requests
+from pubnub import Pubnub
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return 'Hello world'
-
-
-@app.route('/<room_url>/playlist/')
-def playlist(room_url):
-    room_id = get_room_id(room_url)
-    url = 'https://api.dubtrack.fm/room/%s/playlist/history' % room_id
-    r = requests.get(url)
-    return str(r.content)
-
-
-if __name__ == '__main__':
-    app.run()
+from flask import jsonify
 
 def get_room_id(room_url):
+    """Returns id of the room."""
     url = 'https://api.dubtrack.fm/room/%s/' % room_url
     r = requests.get(url)
-    result = r.json()['data']['_id']
-    return str(result)
+    return r.json()['data']['_id']
+
+def get_room_playlist(room_id):
+    """Returns the playlist history of the room (20 songs)."""
+    url = 'https://api.dubtrack.fm/room/%s/playlist/history' % room_id
+    r = requests.get(url)
+    return r.json()
+
+def get_room_active_song(room_id):
+    """Returns the current song."""
+    url = 'https://api.dubtrack.fm/room/%s/playlist/active' % room_id
+    r = requests.get(url)
+    try:
+        if r.json()['data']['song']:
+            return r.json()
+    except KeyError:
+        return None
+
+def get_username_by_id(user_id):
+    """Returns the username."""
+    url = 'https://api.dubtrack.fm/user/%s' % user_id
+    r = requests.get(url)
+    return r.json()
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST' and request.form['room_name']:
+        return redirect(url_for('room_info', room_url=request.form['room_name']))
+    else:
+        return render_template('index.html')
+
+@app.route('/<room_url>/')
+def room_info(room_url):
+    global pubnub
+    channel = 'dubtrackfm-' + room_url
+    pubnub.subscribe(channels=channel, callback=callback, error=callback,
+                 connect=connect, reconnect=reconnect, disconnect=disconnect)
+    print("Suscribed to something")
+    room_id = get_room_id(room_url)
+
+    active_song = None
+    while(active_song == None):
+        active_song = get_room_active_song(room_id)
+
+    room_playlist = get_room_playlist(room_id)
+    active_song_user = get_username_by_id(active_song['data']['song']['userid'])    
+    data = {
+        'active_song' : active_song,
+        'room_playlist': room_playlist,
+        'active_song_user' : active_song_user
+    }
+    return render_template('room.html', data=data)
+
+@app.route('/<room_url>/playlist/')
+def playlist(room_url): 
+    room_id = get_room_id(room_url)
+    result = get_room_playlist(room_id)
+    return jsonify(result)
+
+@app.route('/<room_url>/active/')
+def active(room_url):
+    room_id = get_room_id(room_url)
+    result = get_room_active_song(room_id)
+    return jsonify(result)
+
+
+pubnub = Pubnub(publish_key="", subscribe_key="sub-c-2b40f72a-6b59-11e3-ab46-02ee2ddab7fe")
+
+def callback(message, channel):
+    print(message)
+  
+  
+def error(message):
+    print("ERROR : " + str(message))
+  
+  
+def connect(message):
+    print("CONNECTED")
+    #print(pubnub.publish(channel='my_channel', message='Hello from the PubNub Python SDK'))
+  
+    
+def reconnect(message):
+    print("RECONNECTED")
+  
+  
+def disconnect(message):
+    print("DISCONNECTED")
+  
+
+app.run(debug=True)
+
+if __name__ == '__main__':
+    pass
